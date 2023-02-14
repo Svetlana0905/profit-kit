ea5f01f85eb29dde6e5a08cec5950a44a10e80be
Svetlana <pokrowsckaya.sveta@gmail.com>
2023-02-14 09:10:09 +0300

    change position for btn-prev and btn-next

--- src/js/helpers/elementsNodeList.js
+++ src/js/helpers/elementsNodeList.js
@@ -1,8 +1,8 @@
 const html = document.documentElement
 const body = document.body
 const swiperWrapper = document.querySelector('.swiper-wrapper')
-const nextBtnSwiper = document.querySelector('.btn-block__button-next')
-const prevBtnSwiper = document.querySelector('.btn-block__button-prev')
+const nextBtnSwiper = document.querySelector('.primary-swiper__button-next')
+const prevBtnSwiper = document.querySelector('.primary-swiper__button-prev')
 const pagination = document.querySelector('.swiper-pagination')
 const imgSliderWrapper = document.querySelector('.image-slider__wrapper')
 const list = document.querySelectorAll('.card-link, .promo-nav__link')
